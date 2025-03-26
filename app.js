a5b0befb3e00f8cc3bb20333cbc419c837bff8c5
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2025-03-25 20:34:42 -0500

    Fix: Close Funcion Hamburguer Menu

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 const openMenu = document.querySelector(".hamburgerMenu");
 const nav = document.querySelector(".list_container");
+const closeMenu = document.querySelector(".closeMenu");
 
 openMenu.addEventListener("click", () => {
   nav.classList.add("visible");
+  closeMenu.classList.add("visible");
+  openMenu.classList.replace("hamburgerMenu", "inactive");
   console.log("MenuAbierto");
 });
 
 window.addEventListener("click", (e) => {
-  if (!openMenu.contains(e.target) && !nav.contains(e.target)) {
+  if (
+    !openMenu.contains(e.target) &&
+    !nav.contains(e.target) &&
+    !closeMenu.contains(e.target)
+  ) {
     nav.classList.remove("visible");
+    openMenu.classList.replace("inactive", "hamburgerMenu");
+
     console.log("Menú cerrado");
   }
 });
